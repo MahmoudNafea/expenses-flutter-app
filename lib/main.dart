d465e60829669809904b5b5929554875656dd514
mahmoud <mahmoud@whipmobility.com>
2022-02-07 16:07:38 +0800

    fluter structure and components

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import './widgets/new-transaction.dart';
+import '../widgets/transaction-list.dart';
+import '../models/transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'a1',
+      title: 'Grocery',
+      amount: 89.4,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'a2',
+      title: 'Bills',
+      amount: 50.5,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+    setState(() {
+      transactions.add(newTx);
+    });
+  }
+
+  void startNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
+
+  ///to do proper popup in web we use show model and return alert dialog
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal Expenses'),
+        actions: [
+          IconButton(
+              onPressed: () => startNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Card(
+                child: Text('Chart!'),
+                elevation: 5,
+                color: Colors.blueAccent,
+              ),
+              width: double.infinity,
+            ),
+            TransactionList(transactions),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => startNewTransaction(context),
+      ),
+    );
+  }
+}
